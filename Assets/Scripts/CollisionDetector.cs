03f7926967c13368016fbeb3d031b1e83c2dc354
Levi Biasco <lbiasco@nd.edu>
2016-10-02 03:31:40 -0400

    Collider testing

---
+++ Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDetector : MonoBehaviour {
+
+    public LayerMask collideLayer = 7;
+
+    private bool messageOut = false;
+    private BoxCollider2D colliderPlayer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        colliderPlayer = this.GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (colliderPlayer.IsTouchingLayers(collideLayer) && messageOut == false)
+        {
+            Debug.Log("Touching");
+            messageOut = true;
+        }
+        else
+        {
+            if(!colliderPlayer.IsTouchingLayers(collideLayer) && messageOut == true)
+            {
+                Debug.Log("Not touching");
+                messageOut = false;
+            }
+        }
+	}
+}
