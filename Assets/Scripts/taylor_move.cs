5326a4f0f4b42f5113317fd5daeda554c72e3c5d
TayMurr <tmurray5@nd.edu>
2016-10-02 17:21:02 -0400

    Initial Commit

---
+++ Assets/Scripts/taylor_move.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class taylor_move : MonoBehaviour {
+
+	public float moveDist = 1;
+	public float moveTime = 1;
+
+	private bool isMoving = false;
+	private Vector2 moveDir;
+	private float dt = 0;
+	private Vector2 oldPos;
+
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+
+	void Move()
+	{
+
+
+
+
+		{
+			
+			{
+				moveDir += Vector2.right * Input.GetAxisRaw("Horizontal");
+				moveDir.Normalize();
+				isMoving = true;
+			}
+
+			{
+				moveDir += Vector2.up * Input.GetAxisRaw("Vertical");
+				moveDir.Normalize();
+				isMoving = true;
+			}
+
+			this.transform.Translate (moveDir);
+		}
+
+
+
+
+	}
+}
