eef1a25fdd411a2e205be675390030277f15a088
Levi Biasco <lbiasco@nd.edu>
2016-11-13 17:43:35 -0500

    Kurt scripts added (Door script, Game Manager, and level transitions)

---
+++ Assets/Scripts/doorScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorScript : MonoBehaviour {
+
+    // Door ID within GameManager door dictionaries
+    public int doorID;
+
+    // Positon that player is placed upon entering through door
+    public float entranceOffsetX;
+    public float entranceOffsetY;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Door collision detected!");
+
+        if (other.gameObject.tag == "Player")
+        {
+            GameManagerScript.instance.loadSceneFromDoor(doorID);
+        }
+    }
+}
