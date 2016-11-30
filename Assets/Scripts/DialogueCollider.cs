fddf164fabb7cb3d287ac8674081b4321292a3f7
pkwak1 <pkwak@pauls-mbp.dhcp.nd.edu>
2016-11-29 21:43:49 -0500

    moved dialogue scripts to assets folder

---
+++ Assets/Scripts/DialogueCollider.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueCollider : MonoBehaviour {
+
+	public string Dialogue;
+	private DialogueManager dObject;
+
+	// Use this for initialization
+	void Start () {
+		dObject = FindObjectOfType<DialogueManager>(); //<> is for versatility of multiple sprites/objects
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void onTriggerEnter2D(Collider2D other) //onTriggerStay accounts for every moment in the zone (so that space can be hit anytime, rather than as soon as you enter the zone)
+	{
+		//conditional works by having the name of the player, "Test Main" be in the space of object
+		if (other.gameObject.tag == "Player") //checking if player is entering object space 
+		{ 
+			dObject.canDisplay = true;
+		}
+	}
+
+	void onTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") //checking if player is exiting object space 
+		{ 
+			dObject.canDisplay = false;
+		}
+
+
+	}
+
+	void ShowDialogue()
+	{
+		if (Input.GetKeyUp(KeyCode.Space)) 
+		{
+			dObject.ShowBox(Dialogue);
+		}
+	}
+}
---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DialogueManager : MonoBehaviour {
+		
+	public GameObject dBox; 
+	public Text dText;
+	public bool dialogActive = false; 
+	public GameObject boxText;
+	private BoxCollider2D dialogueZone;
+	private GameObject player;
+
+	public bool canDisplay;
+
+	// Use this for initialization
+	void Start () {
+		dBox.SetActive (false);
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+		canDisplay = true;
+		//dialogueZone = GameObject.FindGameObject.GetComponent<BoxCollider2D>();
+		//what i need: 
+		/* if the player's collision is colliding with the dialogue collider. 
+		 * handle for is touching component of the colliders to be true
+		 * Then set text to hi, and make sure that it's versatile with other objects and unique texts*/ 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (dialogActive == false && Input.GetKeyDown(KeyCode.Space) && canDisplay)
+		{
+			dBox.SetActive(true); //deactivate box
+			dialogActive = true;
+			dText.GetComponent<Text>().text = "Hi!";
+
+		}
+		else if (dialogActive == true && Input.GetKeyDown(KeyCode.Space))
+		{
+			dBox.SetActive(false); //deactivate box
+			dialogActive = false;
+		}
+	}
+
+	// For other text to show
+	public void ShowBox(string Dialogue)
+	{
+		dText.text = Dialogue;
+		dialogActive = true;
+		dBox.SetActive (true);
+	}
+
+	public void DisplayDialogue(int index)
+	{
+		
+	}
+}
