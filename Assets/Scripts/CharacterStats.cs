9da541d9eb5b1d9a20e8906a23b42c68c5c679a3
Levi Biasco <lbiasco@nd.edu>
2016-12-08 02:06:30 -0500

    Inital combat system

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterStats : MonoBehaviour {
+
+    public struct Move
+    {
+        bool unique;
+        bool special;
+        int power;
+        int manaUsage;
+    }
+
+    public enum Type : int { FYS, Arch, AL, Bus, Eng, Sci };
+
+    public int hp, atk, def, spec, lck, mana;
+    public Type type;
+
+    public Move[] moveset = new Move[4];
+
+    [SerializeField]
+    private bool randomize;
+    [SerializeField]
+    private int randomLevel = 1;
+
+    // enum for Majors, Dorms?
+
+	void Start ()
+    {
+        if(randomize)
+            GenerateRandom(randomLevel);
+	}
+	
+	void Update ()
+    {
+	
+	}
+    
+    void GenerateRandom(int level)
+    {
+        type = (Type)Random.Range(0, 6);
+
+        //int hpMax, hpMin;
+
+        if(type == Type.FYS)
+        {
+            
+        }
+
+        hp = Random.Range(5 * level, 10 * level);
+        atk = Random.Range(5 * level, 10 * level);
+        def = Random.Range(5 * level, 10 * level);
+        spec = Random.Range(5 * level, 10 * level);
+        lck = Random.Range(5 * level, 10 * level);
+        mana = Random.Range(5 * level, 10 * level);
+    }
+}
---
+++ Assets/Scripts/CombatManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CombatManager : MonoBehaviour {
+
+    public static CombatManager instance;
+
+    public List<CharacterStats> playerStats;
+    public List<CharacterStats> enemyStats;
+
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+        else Destroy(this);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Action();
+        }
+    }
+
+    void Action()
+    {
+        //enemyStats[0].hp -= playerStats[0].atk;
+    }
+}
